1182d6b29b2c94530608565c06e46d4858579880
Diego Matias <dm.diego.bh@gmail.com>
2017-06-08 22:02:08 -0300

    create theme

---
+++ wp-content/themes/curriculum/footer.php
@@ -0,0 +1,6 @@
+<?php wp_footer() ?>
+<!-- Scripts -->
+  <script src="<?php echo get_template_directory_uri()?>/assets/js/jquery.min.js"></script>
+  <script src="<?php echo get_template_directory_uri()?>/assets/js/skel.min.js"></script>
+  <script src="<?php echo get_template_directory_uri()?>/assets/js/main.js"></script>
+  <script src="<?php echo get_template_directory_uri()?>/assets/js/galery.js"></script>
---
+++ wp-content/themes/curriculum/header.php
@@ -0,0 +1,9 @@
+<head>
+  <?php wp_head() ?>
+  <title>Diego Matias de Oliveira</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/main.css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/custom.css" />
+  <noscript><link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/noscript.css" /></noscript>
+</head>
