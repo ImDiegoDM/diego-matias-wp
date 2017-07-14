b3b562d0fc40844b2e0878537b8f236317b26b20
Diego Matias <dm.diego.bh@gmail.com>
2017-07-14 11:05:08 -0300

    optimize css

--- wp-content/themes/curriculum/header.php
+++ wp-content/themes/curriculum/header.php
@@ -4,9 +4,9 @@
   <meta charset="utf-8" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/bootstrap.min.css" />
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/main.css" />
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/custom.css" />
-  <noscript><link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/noscript.css" /></noscript>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/main.min.css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/custom.min.css" />
+  <noscript><link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/noscript.min.css" /></noscript>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/jquery.min.js"></script>
   <script src="<?php echo get_template_directory_uri()?>/assets/js/jquery.validate.min.js"></script>
 </head>
